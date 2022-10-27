7c98ae6d0faf8779a61a004909d76f63336a651e
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2022-10-27 19:17:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -32,20 +32,77 @@ shareBtn.addEventListener('click', event => {
 
 
 
-
- function modal_05() {
+//************************************************************************************************* */
+//Numero de pagina
+function modal_009() {
     Swal.fire({
-        html: '<h3 class="bm">Black Momets</h3>',
-        imageUrl: 'https://res.cloudinary.com/lhuis/image/upload/v1666754485/iconos/fkppmxoe9qs9xhggmzcc.png',
-       //tamaño
+        html: '<h3 class="bm">ivan zarate Foto2</h3>',
+        //tamaño
         imageWidth: 300,
         imageHeight: 300,
         //cerrar
-        padding:'1rem',
-        showCloseButton:true,
-        showConfirmButton:false,
-        background:'linear-gradient(#000,#000)',
-        
+        padding: '1rem',
+        showCloseButton: true,
+        showConfirmButton: false,
+        background: 'linear-gradient(#000,#000)',
+        //ajustes
+        showCloseButton: true,
+        timer: '5000',
+        timerProgressBar: true,
+        customClass: {
+            actions: 'content',
+        }
     })
- }
- 
+}
+
+
+
+
+function modal_05() {
+    Swal.fire({
+        html: '<h3  onclick="modal_009();" class="bm">Black Momets</h3>',
+        imageUrl: 'https://res.cloudinary.com/lhuis/image/upload/v1666754485/iconos/fkppmxoe9qs9xhggmzcc.png',
+        //tamaño
+        imageWidth: 300,
+        imageHeight: 300,
+        //cerrar
+        padding: '1rem',
+        showCloseButton: true,
+        showConfirmButton: false,
+        background: 'linear-gradient(#000,#000)',
+    })
+}
+
+// automatica
+setTimeout(function () {
+Swal.fire({
+    
+    html: '<div class="mensaje">' +
+        '<h3 class="bm">Black Momets</h3>' +
+        '<p>Foto disponible el <b>4 de octubre</b>, Si no aparecen escribeme al </p>' +
+        '<a href="https://wa.me/541144382987" target="_blank"><button class="bt-msj" >+54 11 4438-2987</button></a>' +
+        '</div>',
+     /*
+        html: '<div class="mensaje">' +
+        '<h3 class="bm">Black Momets</h3>' +
+        '<p>Fotos listas, gracias por la confianza</p>' +
+        
+        '</div>',
+    */
+    //tamaño
+    imageWidth: 300,
+    imageHeight: 300,
+    //cerrar
+    padding: '1rem',
+    showCloseButton: true,
+    showConfirmButton: false,
+    background: 'linear-gradient(#000,#000)',
+    //ajustes
+    showCloseButton: true,
+    timer: '5000',
+    timerProgressBar: true,
+    customClass: {
+        actions: 'content',
+    }
+})
+}, 1000 * 2);
